e124f1ac52e76c49ba5ad608c38561c63c020625
Ritesh T <riteshtch@gmail.com>
2020-04-10 10:38:14 -0700

    Update MQ131.cpp

--- MQ131.cpp
+++ MQ131.cpp
@@ -2,8 +2,6 @@
  * Arduino-MQ131-driver                                                       *
  * --------------------                                                       *
  * Arduino driver for gas sensor MQ131 (O3)                                   *
- * Author: Olivier Staquet                                                    *
- * Last version available on https://github.com/ostaquet/Arduino-MQ131-driver *
  ******************************************************************************/
 
  #include "MQ131.h"
